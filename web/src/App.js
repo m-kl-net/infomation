6422588300e81b7876a843d80f5ff8dd4b2ea7cf
very-hard-name <84565053+very-hard-name@users.noreply.github.com>
2021-05-24 15:09:39 +0900

    Naver Map update

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {RenderAfterNavermapsLoaded, NaverMap} from "react-naver-maps"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App(props) {
+
+
+  return(
+    <div>
+
+
+
+<RenderAfterNavermapsLoaded
+  ncpClientId={"n7agakswot"}
+>
+  <NaverMap 
+    mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+    style={{
+      width: '100%',
+      height: '400px',
+    }}
+    defaultCenter={{ lat: 37.3595704, lng: 127.105399 }}
+    defaultZoom={10}
+  />
+  </RenderAfterNavermapsLoaded>
+      
     </div>
-  );
+  )
 }
-
-export default App;
