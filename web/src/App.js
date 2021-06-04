12896aa5f1ccd9750cc76b5c04268920c8dab238
m-kl-net <moveskill@klnet.co.kr>
2021-06-04 15:56:05 +0900

    test

--- web/src/App.js
+++ web/src/App.js
@@ -1,28 +1,31 @@
+import React,{useEffect,useState} from "react";
 import {RenderAfterNavermapsLoaded, NaverMap} from "react-naver-maps"
 import config from "./common/config.js"
-
-export default function App(props) {
-
-
+import axios from "axios"
+export default function App(callback) {
+  const [zoom, setZoom] = useState(2);
+  const [position, setPosition] = useState({lat:37.3595704,lng:127.105399})
   return(
-    <div>
-
-
-
-<RenderAfterNavermapsLoaded
-  ncpClientId={config.naverMapApi}
->
-  <NaverMap 
-    mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
-    style={{
-      width: '100%',
-      height: '400px',
-    }}
-    defaultCenter={{ lat: 37.3595704, lng: 127.105399 }}
-    defaultZoom={10}
-  />
-  </RenderAfterNavermapsLoaded>
-      
+    <div style={{height:'100vh'}}>
+      <RenderAfterNavermapsLoaded
+        ncpClientId={config.naverMapApi}>
+          
+        <NaverMap 
+          mapDivId={'react-naver-map'} // default: react-naver-map
+          style={{
+            width: '100%',
+            height:'100%'
+          }}
+          center={position}
+          zoom={zoom}
+          zoomControl={true}
+          zoomControlOptions={2}
+          onZoomChanged = {(e)=> {setZoom(e)}}
+          onCenterChanged = {(e) => {setPosition({lat:e._lat,lng:e._lng})}}
+          onMouseMove={(e)=>{console.log(e)}}
+          
+          />
+      </RenderAfterNavermapsLoaded>
     </div>
   )
 }
