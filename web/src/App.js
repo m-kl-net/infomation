c25e66ba1f60d324e36b71ada6001c37811d9da4
m-kl-net <moveskill@klnet.co.kr>
2021-08-06 15:59:00 +0900

    edit

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,16 @@
 import React,{useEffect,useState} from "react";
 import {RenderAfterNavermapsLoaded, NaverMap} from "react-naver-maps"
-import config from "./common/config.js"
+import {BrowserRouter, Route, Redirect, Switch, Link} from "react-router-dom"
 import axios from "axios"
+import config from "./env.json"
 export default function App(callback) {
   const [zoom, setZoom] = useState(2);
   const [position, setPosition] = useState({lat:37.3595704,lng:127.105399})
+
+  useEffect(()=>{
+
+    
+  },[])
   return(
     <div style={{height:'100vh'}}>
       <RenderAfterNavermapsLoaded
